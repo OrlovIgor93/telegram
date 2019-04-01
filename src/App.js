982215cd0d9e75fa516b97d85c871b4f3cba52da
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-01 22:14:12 +0300

    Change App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import WrapPage from './contaniers/wrapPage'
+import AboutPage from './components/about/about'
+import LoginPage from './components/login/loginPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <WrapPage />
+        <AboutPage />
       </div>
     );
   }
