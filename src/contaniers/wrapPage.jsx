7fa0fe39bee0f6f7ff2c724f9ab68984f4283735
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-01 15:37:18 +0300

    Change style and view main page

--- src/contaniers/wrapPage.jsx
+++ src/contaniers/wrapPage.jsx
@@ -7,8 +7,8 @@ const wrapPage = () => {
       <div className="wrapHeader" />
       <div className="wrapPageContext">
         <div className="leftBlockDialogs">
-          <div className="search">
-            <input />
+          <div className="searchWrap">
+            <input type="text" className="inputSearch" placeholder="Search..." />
           </div>
           <ul className="dialogs">
             <li>
@@ -106,44 +106,44 @@ const wrapPage = () => {
                 </div>
               </div>
             </li>
-                      <li>
-                          <div className="dialog">
-                              <div className="imgDialogPhoto">
-                                  <span className="textPlaceholder">DP</span>
-                              </div>
-                              <div className="dialogInfo">
-                                  <div className="dialogName">Dima Petrov</div>
-                                  <div className="dialogLastMessage">
-                                      You: Ok. See you later..
+            <li>
+              <div className="dialog">
+                <div className="imgDialogPhoto">
+                  <span className="textPlaceholder">DP</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Dima Petrov</div>
+                  <div className="dialogLastMessage">
+                    You: Ok. See you later..
                   </div>
-                              </div>
-                              <div className="infoLastMessage">
-                                  <div>8:23 PM</div>
-                                  <div className="chip">
-                                      <div className="chipText">5</div>
-                                  </div>
-                              </div>
-                          </div>
-                      </li>
-                      <li>
-                          <div className="dialog">
-                              <div className="imgDialogPhoto">
-                                  <span className="textPlaceholder">DP</span>
-                              </div>
-                              <div className="dialogInfo">
-                                  <div className="dialogName">Dima Petrov</div>
-                                  <div className="dialogLastMessage">
-                                      You: Ok. See you later..
+                </div>
+                <div className="infoLastMessage">
+                  <div>8:23 PM</div>
+                  <div className="chip">
+                    <div className="chipText">5</div>
                   </div>
-                              </div>
-                              <div className="infoLastMessage">
-                                  <div>8:23 PM</div>
-                                  <div className="chip">
-                                      <div className="chipText">5</div>
-                                  </div>
-                              </div>
-                          </div>
-                      </li>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="dialog">
+                <div className="imgDialogPhoto">
+                  <span className="textPlaceholder">DP</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Dima Petrov</div>
+                  <div className="dialogLastMessage">
+                    You: Ok. See you later..
+                  </div>
+                </div>
+                <div className="infoLastMessage">
+                  <div>8:23 PM</div>
+                  <div className="chip">
+                    <div className="chipText">5</div>
+                  </div>
+                </div>
+              </div>
+            </li>
             <li>
               <div className="dialog">
                 <div className="imgDialogPhoto">
