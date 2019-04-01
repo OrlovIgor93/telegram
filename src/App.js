8b88a59ada6b83821d0b9ea0a47aebd00ed9ff03
MenkuiChan <ilonaspolding@gmail.com>
2019-04-01 23:23:29 +0300

    add jsx file for login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import LoginPage from './components/login/loginPage'
 import WrapPage from './contaniers/wrapPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <WrapPage />
+        <WrapPage />
+        <LoginPage />
       </div>
     );
   }
