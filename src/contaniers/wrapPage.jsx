ff4d1e20b7a2ac31f18a67816a66ec0612a6c3f2
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-01 20:28:56 +0300

    Add more elements in main page

--- src/contaniers/wrapPage.jsx
+++ src/contaniers/wrapPage.jsx
@@ -8,7 +8,11 @@ const wrapPage = () => {
       <div className="wrapPageContext">
         <div className="leftBlockDialogs">
           <div className="searchWrap">
-            <input type="text" className="inputSearch" placeholder="Search..." />
+            <input
+              type="text"
+              className="inputSearch"
+              placeholder="Search..."
+            />
           </div>
           <ul className="dialogs">
             <li>
@@ -241,7 +245,208 @@ const wrapPage = () => {
             </li>
           </ul>
         </div>
-        <div className="history" />
+
+        <div className="history">
+          <div className="inputArea">
+            <div className="imgDialogPhoto inputLogoImage">
+              <span className="textImagePlaceholder">DP</span>
+            </div>
+            <textarea
+              type="textArea"
+              className="inputMessage"
+              rows="2"
+              cols="50"
+              placeholder="Message..."
+            />
+            <div className="imgDialogPhoto inputLogoImage imageGroup">
+              <span className="textImagePlaceholder">LS</span>
+            </div>
+          </div>
+
+          <ul className="messageContext">
+            <li>
+              <div style={{ height: 30 }} />
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">DP</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Dima Petrov</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Harum totam numquam molestiae excepturi est possimus facere
+                    magnam corporis eum, non nesciunt ab dolores repudiandae
+                    voluptatem corrupti unde voluptatum. Non, excepturi.
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:58:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">LS</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Liza Smitt</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Nobis, iusto velit commodi mollitia eveniet aspernatur,
+                    rerum dolor deleniti quam autem iste eius repellat doloribus
+                    laborum voluptates eos explicabo tempore doloremque!
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:57:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">DP</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Dima Petrov</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Harum totam numquam molestiae excepturi est possimus facere
+                    magnam corporis eum, non nesciunt ab dolores repudiandae
+                    voluptatem corrupti unde voluptatum. Non, excepturi.
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:56:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">LS</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Liza Smitt</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur!
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:55:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">DP</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Dima Petrov</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Harum totam numquam molestiae excepturi est possimus facere
+                    magnam corporis eum, non nesciunt ab dolores repudiandae
+                    voluptatem corrupti unde voluptatum. Non, excepturi.
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:54:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">LS</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Liza Smitt</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur!
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:53:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">DP</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Dima Petrov</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Harum totam numquam molestiae excepturi est possimus facere
+                    magnam corporis eum, non nesciunt ab dolores repudiandae
+                    voluptatem corrupti unde voluptatum. Non, excepturi.
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:52:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">LS</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Liza Smitt</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur!
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:51:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">DP</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Dima Petrov</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Harum totam numquam molestiae excepturi est possimus facere
+                    magnam corporis eum, non nesciunt ab dolores repudiandae
+                    voluptatem corrupti unde voluptatum. Non, excepturi.
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:50:57 PM</div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="message">
+                <div className="imgDialogPhoto imageUserMessage">
+                  <span className="imageUserMessage">LS</span>
+                </div>
+                <div className="dialogInfo">
+                  <div className="dialogName">Liza Smitt</div>
+                  <div className="dialogLastMessage">
+                    Lorem ipsum dolor sit amet consectetur!
+                  </div>
+                </div>
+                <div className="timeLastMessage">
+                  <div>6:07:57 PM</div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
