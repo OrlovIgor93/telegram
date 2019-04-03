1753a8a67f1215c0c1e1b520794f57c1587510d1
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-03 10:47:32 +0300

    Change avatar component

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './style.css'
 
-export const Avatar = ({children, ...attrs})=>{
+export const Avatar = ({ children, className, ...attrs})=>{
+
+    const classes = classNames(
+        'avatar',
+        className,   
+    );
+
 const Tag = attrs.src ? 'img':'p';
 
     return(
-        <Tag className="avatar"
+        <Tag className={classes}
             {...attrs}
-        >
-            {children}
-        </Tag>
+         >
+         {children}
+         </Tag>
+     
     );
 };
