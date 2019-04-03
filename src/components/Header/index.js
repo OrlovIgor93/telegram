76b86ff7f516dd16401f790bb124645c00ba557f
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-03 20:38:27 +0300

    Fix navigation on footer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 export const Header = () => (
-  <div className="wrapHeader">
-    <div className="headerLeftMenu">
+         <div className="wrapHeader">
+           <div className="headerLeftMenu">
+             <div className="navButton">
+               <Link to="/about">About</Link>
+             </div>
       <div className="navButton">
-        <Link to="/about">About</Link>
-      </div>
+               <Link to="/contact">User</Link>
+             </div>
       <div className="navButton">
-        <Link to="/contact">User</Link>
-      </div>
-      <div className="navButton">
-        <Link to="login">Login</Link>
-      </div>
-    </div>
-    <div className="headerTitle">
-      <h2>Liza Smitt</h2>
-    </div>
-  </div>
-);
+               <Link to="login">Login</Link>
+             </div>
+           </div>
+           <div className="headerTitle">
+             <h2>Liza Smitt</h2>
+           </div>
+         </div>
+       );
