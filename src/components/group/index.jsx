e40b351318d626e6e66fb188c4f56d288b5d10ab
MenkuiChan <ilonaspolding@gmail.com>
2019-04-03 17:01:03 +0300

    beautify group page

--- src/components/group/index.jsx
+++ src/components/group/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Avatar } from "../Avatar";
+import "./style.css";
 import "../MainPage/style.css";
 
 export const GroupPage = () => (
-    <div className="head">
-        <div className="wrap">
+    <div className="head3">
+        <div className="wrap3">
             <div className="title_wrap">
                 <div className="new-group">New Group</div>
                 <div className="close">Close</div>
@@ -14,12 +15,35 @@ export const GroupPage = () => (
                 <div className="input_search"><input type="search" placeholder="Search..." /></div>
             </div>
             <div className="contact_wrap">
-                <ul className="dialogs">
+                <br /> <br /> <br /> <br />
+                <ul className="dialogs dialogs3">
                     <li>
                         <div className="dialog">
                             <Avatar style={{ height: 48, width: 48, backgroundColor: "coral" }}>im</Avatar>
                             <div className="dialogInfo">
                                 <div className="dialogName">Ilona Menkui</div>
+                            </div>
+                            <div className="infoLastMessage">
+                                <div>Last seen recently</div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dialog">
+                            <Avatar style={{ height: 48, width: 48, backgroundColor: "coral" }}>dp</Avatar>
+                            <div className="dialogInfo">
+                                <div className="dialogName">Dima Platon</div>
+                            </div>
+                            <div className="infoLastMessage">
+                                <div>Last seen recently</div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dialog">
+                            <Avatar style={{ height: 48, width: 48, backgroundColor: "coral" }}>lm</Avatar>
+                            <div className="dialogInfo">
+                                <div className="dialogName">Lera Milasha</div>
                             </div>
                             <div className="infoLastMessage">
                                 <div>Last seen recently</div>
