e9ca2c6a73e773997a89366c92a22563b80d7dce
Valerie <vallerie8@gmail.com>
2019-04-03 20:14:48 +0300

    added 3 AboutPage components (Heading,ModalBody,ModalWrapper)

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,36 +1,10 @@
 import React from "react";
 import "./style.css";
+import {ModalWrapper} from "./ModalWrapper";
 
 export const About = () => {
     return (
-<div className="wrap">
-    <div className="head">
-        <div className="title">
-            <div className="title1">About</div>
-            <div className="title2">Close</div>
-        </div>
-        <div className="photo_wrap">
-            <div className="telegram_logo">
-                <img src="https://web.telegram.org/img/Telegram72.png" alt="logo"/>
-            </div>
-            <div className="profile">
-                <div className="profile_name">Epam Telegram</div>
-                <div className="profile_description">Version 0.1</div>
-            </div>
-        </div>
-    </div>
-    <div className="profile_wrap">
-        <div className="section">
-            <p>Just another free messaging app based on Telegram API made by three future JS geniuses.</p>
-        </div>
-    </div>
-    <div className="footer_wrap">
-        <div className="footer">
-            <a className="link1" href="/" target="_blank">Link 1</a>
-            <a className="link2" href="/" target="_blank">Link 2</a>
-        </div>
-    </div>
-</div>
-);
+       <ModalWrapper/>
+   );
 };
 
