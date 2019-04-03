3231c5f40308c62104634a75d6de70749f0732e5
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-03 20:54:57 +0300

    Fix navigate in app

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,20 +4,23 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 export const Header = () => (
-         <div className="wrapHeader">
-           <div className="headerLeftMenu">
-             <div className="navButton">
-               <Link to="/about">About</Link>
-             </div>
+  <div className="wrapHeader">
+    <div className="headerLeftMenu">
       <div className="navButton">
-               <Link to="/contact">User</Link>
-             </div>
+        <Link to="/about">About</Link>
+      </div>
       <div className="navButton">
-               <Link to="login">Login</Link>
-             </div>
-           </div>
-           <div className="headerTitle">
-             <h2>Liza Smitt</h2>
-           </div>
-         </div>
-       );
+        <Link to="/contact">User</Link>
+      </div>
+      <div className="navButton">
+        <Link to="contact">Contact</Link>
+      </div>
+      <div className="navButton">
+        <Link to="group">Group</Link>
+      </div>
+    </div>
+    <div className="headerTitle">
+      <h2>Liza Smitt</h2>
+    </div>
+  </div>
+);
