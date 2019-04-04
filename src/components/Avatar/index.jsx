3f62bdf1a8237b4d2ae13b62484f9f169a76975c
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-05 01:48:32 +0300

    Change avatar component for text

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -8,9 +8,20 @@ export const Avatar = ({ children, className, ...attrs }) => {
 
   const Tag = attrs.src ? "img" : "p";
 
+  const getUserLetter = () => {
+    const arrWord = children.split(" ");
+    console.log(arrWord);
+    const letters =
+      arrWord.length === 1
+        ? arrWord[0][0] + arrWord[0][1]
+        : arrWord[0][0] + arrWord[1][0];
+    return letters;
+  };
+
+  const userLeter = Tag === "p" ? getUserLetter() : children;
   return (
     <Tag className={classes} {...attrs}>
-      {children}
+      {userLeter}
     </Tag>
   );
 };
