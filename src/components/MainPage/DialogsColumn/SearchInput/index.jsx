9a1f260a8d68b7ffad16958fba1fb30480c44ab1
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-05 04:48:35 +0300

    change search

--- src/components/MainPage/DialogsColumn/SearchInput/index.jsx
+++ src/components/MainPage/DialogsColumn/SearchInput/index.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import "./style.css";
 
 export const SearchInput = () => (
-
-    <input type="text" className="inputSearch" placeholder="Search..." />
-
-);
+         <input
+           type="text"
+           className="input-search"
+           placeholder="Search..."
+         />
+       );
