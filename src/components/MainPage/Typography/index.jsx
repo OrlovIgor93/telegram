d8cdd6ee9075289c6cef6fb3da4085e0a05f1ae8
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-07 23:52:44 +0300

    Add typography component and use it in other components

---
+++ src/components/MainPage/Typography/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const Typography = ({variant , children, style }) => {
+    let options={};
+    (() => {
+      switch (variant) {
+        case "title":
+              options= { fontSize: 13, fontWeight: 700 };
+          break;
+        case "title2":
+              options= { fontSize: 12, fontWeight: 700 };
+          break;
+        case "caption":
+              options = { fontSize: 11, fontWeight: 400 };
+          break;
+        case "body":
+              options = { fontSize: 13, fontWeight: 400 };
+          break;
+        default:
+        
+          break;
+      }
+    })();
+    let styleText = { ...options, ...style };
+    return <span style={styleText}>{children}</span>;
+};
+
