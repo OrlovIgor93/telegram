21a5b032634aebfc815326fdaad48a5cbb62943d
MenkuiChan <ilonaspolding@gmail.com>
2019-04-08 12:26:49 +0300

    fix contacts page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import { User } from './components/User'
 import { MainPage } from './components/MainPage'
 import { About } from './components/About'
+import { Contacts } from "./components/Contacts"
 import { GroupPage } from "./components/Group"
 import { LoginPage } from "./components/Login";
 import { Settings } from "./components/Settings";
@@ -16,6 +17,7 @@ class App extends Component {
         <Route exact path="/" component={MainPage} />
         <Route path="/contact" component={User} />
         <Route path="/about" component={About} />
+        <Route path="/contacts" component={Contacts} />
         <Route path="/login" component={LoginPage} />
         <Route path="/group" component={GroupPage} />
         <Route path="/settings" component={Settings} />
