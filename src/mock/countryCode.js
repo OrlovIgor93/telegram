3bd5b0733b352d9833e0374801b2fc10aeaf67a1
Dmitry Platonov <platonowdmitry@gmail.com>
2019-04-20 00:04:39 +0300

    add mockCountry data

---
+++ src/mock/countryCode.js
@@ -0,0 +1,28 @@
+export const countryCode = [
+    {
+        "code": "+7 840",
+        "name": "Abkhazia",
+        "phoneLenght": 7
+    },
+    {
+        "code": "+93",
+        "name": "Afghanistan",
+        "phoneLenght": 9
+    },
+    {
+        "code": "+355",
+        "name": "Albania",
+        "phoneLenght": 8
+    },
+
+    {
+        "code": "+375",
+        "name": "Belarus",
+        "phoneLenght": 9
+    },
+    {
+        "code": "+32",
+        "name": "Belgium",
+        "phoneLenght": 10
+    },
+]
