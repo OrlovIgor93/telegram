042898ef68f1f56835656cd6da2a1ec2505a18b1
MenkuiChan <ilonaspolding@gmail.com>
2019-04-29 20:51:58 +0300

    fix contacts list

--- src/components/Contacts/ComponentsContacts/ContactList.jsx
+++ src/components/Contacts/ComponentsContacts/ContactList.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Style from '../style.js'
 import Contact from './Contact'
 
-const makeContactComponent = ({ name, imgUrl, status, showDelete }) => (
-    <Contact imgUrl={imgUrl} name={name} showDelete={showDelete} status={status} />
+const makeContactComponent = ({ name, imgUrl, status, showDelete, id }) => (
+    <Contact key={id} imgUrl={imgUrl} name={name} showDelete={showDelete} status={status} />
 )
 
 export const ContactList = ({ editMode = false, contacts = [] }) => (
     <div /*className="contact_wrap"*/>
         <ul className="dialogs" style={Style.dialogsGroupClass}>
             {contacts.map(
-                contact => makeContactComponent({ ...contact, showDelete: editMode })
+                (contact, index) => makeContactComponent({ ...contact, showDelete: editMode, id: index })
             )}
         </ul>
     </div>
